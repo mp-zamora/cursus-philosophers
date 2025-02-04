ca11008c58dadfad6d8c249355b480317ba25870
mp-zamora <mpzamora@mpzamora.com>
2025-02-04 14:41:15 +0100

    Add new philo structure

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 21:07:32 by mpenas-z          #+#    #+#             */
-/*   Updated: 2025/02/04 10:32:09 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:14:37 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,18 +19,27 @@
 
 typedef struct s_philo
 {
-	int	number_of_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	eat_to_finish;
+	int				number;
+	int				status;
+	struct s_philo	*next;
+	struct s_philo	*prev;
 }	t_philo;
 
+typedef struct s_data
+{
+	t_philo	*philo_list;
+	int		number_of_philos;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		eat_to_finish;
+}	t_data;
+
 /* INITIALIZER */
-void	initialize_philo(int argc, char **argv, t_philo *data);
+void	initialize_data(int argc, char **argv, t_data *data);
 
 /* MAIN */
-void	ft_error(char *err_msg, t_philo *data);
-void	free_philo(t_philo *data);
+void	ft_error(char *err_msg, t_data *data);
+void	free_philo(t_data *data);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 10:17:12 by mpenas-z          #+#    #+#             */
-/*   Updated: 2025/02/04 10:27:16 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2025/02/04 14:14:47 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	main(int agrc, char *argv[])
 {
-	t_philo	*data;
+	t_data	*data;
 
 	if (agrc != 5 && argc != 6)
 		return (ft_putstr_fd("Not enough arguments!\n", 2), 1);
