69a3f734749b163d641c1fb7f9c59ddf5aa2740b
mp-zamora <mpzamora@mpzamora.com>
2025-02-04 10:33:13 +0100

    Add base structure

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 21:07:32 by mpenas-z          #+#    #+#             */
-/*   Updated: 2025/02/04 10:18:21 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2025/02/04 10:32:09 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,15 @@
 
 typedef struct s_philo
 {
-	char  *empty;
+	int	number_of_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	eat_to_finish;
 }	t_philo;
+
+/* INITIALIZER */
+void	initialize_philo(int argc, char **argv, t_philo *data);
 
 /* MAIN */
 void	ft_error(char *err_msg, t_philo *data);
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,18 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 10:17:12 by mpenas-z          #+#    #+#             */
-/*   Updated: 2025/02/04 10:17:51 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2025/02/04 10:27:16 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-int	main(void)
+int	main(int agrc, char *argv[])
 {
+	t_philo	*data;
+
+	if (agrc != 5 && argc != 6)
+		return (ft_putstr_fd("Not enough arguments!\n", 2), 1);
+	initialize_philo(argc, argv, &data);
 	printf("¡Hola mundo!\n");
 }
