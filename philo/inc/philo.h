954c23ada023679514e31b52624181cb9e643032
mp-zamora <mpzamora@mpzamora.com>
2025-02-06 09:34:50 +0100

    Expand general structure

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/09 21:07:32 by mpenas-z          #+#    #+#             */
-/*   Updated: 2025/02/05 12:39:03 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2025/02/06 09:33:42 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 /* DEFINE STATUS CODES */
 # define SUCCESS 0
 # define FAILURE 1
+# define FALSE 0
+# define TRUE 1
 # define EXIT_FAILURE 1
 # define EXIT_SUCCESS 0
 
@@ -23,6 +25,7 @@
 # include "../libft/libft.h"
 # include "../libft/get_next_line.h"
 # include <stdio.h>
+# include <sys/time.h>
 
 /* DEFINE DATA STRUCTURES */
 typedef struct s_philo
@@ -30,22 +33,23 @@ typedef struct s_philo
 	int				number;
 	int				status;
 	int				times_eaten;
-	int				last_eat_timestamp;
-	int				last_change_timestamp;
+	long			last_eat_milis;
+	long			last_change_milis;
 	pthread_t		*thread;
+	t_data			*data;
 	struct s_philo	*next;
 }	t_philo;
 
 typedef struct s_data
 {
 	int				number_of_philos;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
 	int				eat_to_finish;
 	int				*forks;
 	t_philo			*philo_list;
-	pthread_mutex_t	*data_mutex;
+	pthread_mutex_t	*mutex;
 }	t_data;
 
 /* INITIALIZER */
@@ -55,7 +59,6 @@ void	initialize_philo_data(int argc, char **argv, t_data *data);
 void	ft_error(char *err_msg, t_data *data);
 int		monitor_philosophers(t_data *data);
 int		launch_philo_threads(t_data *data);
-int		run_simulation(t_data *data);
 void	free_philo_data(t_data *data);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,33 +6,133 @@
 /*   By: mpenas-z <mpenas-z@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 10:17:12 by mpenas-z          #+#    #+#             */
-/*   Updated: 2025/02/05 12:34:09 by mpenas-z         ###   ########.fr       */
+/*   Updated: 2025/02/06 09:34:11 by mpenas-z         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
+/* AUXILIARY */
+
+void	initialize_philo_data(int argc, char *argv[], t_data *data)
+{
+	int	i;
+
+	data = (t_data *)malloc(sizeof(data));
+	if (!data)
+		ft_error("Failure initializing the data structure.", data);
+	data->number_of_philos = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6 && argv[5])
+		data->eat_to_finish = ft_atoi(argv[5]);
+	else
+		data->eat_to_finish = -1;
+	data->forks = (int *)malloc(sizeof(int) * data->number_of_philos);
+	if (!data->forks)
+		ft_error("Failure initializing the data structure.", data);
+	i = -1;
+	while (++i < data->number_of_philos)
+		data->forks[i] = 0;
+	data->philo_list = NULL;
+	/* SHould probably initialize MUTEX here */
+	data->mutex = NULL;
+}
+
+long	get_current_milis(t_data *data)
+{
+	struct timeval	time;
+
+	if (gettimeofday(&time, NULL) == -1)
+		ft_error("Failure fetching current time.", data);
+	return ((time.tv_sec * 1000) + (time.tv_usec / 1000));
+
+}
+
+int	kill_philosopher(t_philo *philo, t_data *data)
+{
+	(void)philo;
+	(void)data;
+	return (FAILURE);
+}
+
+int	did_philosophers_eat_enough(t_data *data)
+{
+	t_philo	*iter;
+
+	iter = data->philo_list;
+	while (iter)
+	{
+		if (iter->times_eaten == data->eat_to_finish)
+			iter = iter->next;
+		else
+			return (FALSE);
+	}
+	return (TRUE);
+}
+
+/* SIMULATION */
+
 int	monitor_philosophers(t_data *data)
 {
-	(void)data;
-	return (1);
+	t_philo		*iter;
+	long		current_milis;
+
+	while (1)
+	{
+		usleep(5000);
+		iter = data->philo_list;
+		while (iter)
+		{
+			current_milis = get_current_milis(data);
+			if (current_milis - iter->last_eat_milis > data->time_to_die)
+				return (kill_philosopher(iter, data));
+			iter = iter->next;
+		}
+		if (did_philosophers_eat_enough(data))
+			return (SUCCESS);
+	}
+	return (FAILURE);
+}
+
+void	*philo_routine(void *arg)
+{
+	t_philo *philo;
+
+	philo = (t_philo *)arg;
+	while (1)
+	{
+		go_sleep(philo);
+		go_think(philo);
+		go_eat(philo);
+	}
 }
 
 int	launch_philo_threads(t_data *data)
 {
-	(void)data;
+	t_philo	*iter;
+	int		num;
+	long	current_milis;
+
+	iter = data->philo_list;
+	num = 0;
+	while (iter && num <= data->number_of_philos)
+	{
+		iter->number = ++num;
+		iter->status = 0;
+		iter->times_eaten = 0;
+		current_milis = get_current_milis(data);
+		iter->last_eat_milis = current_milis;
+		iter->last_change_milis = current_milis;
+		iter->data = data;
+		pthread_create(iter->thread, NULL, philo_routine, (void *)iter);
+		iter = iter->next;
+	}
 	return (1);
 }
 
-int	run_simulation(t_data *data)
-{
-	int	exit_status;
-
-	exit_status = launch_philo_threads(data);
-	if (exit_status == 0)
-		exit_status = monitor_philosophers(data);
-	return (exit_status);
-}
+/* MAIN */
 
 void	ft_error(char *err_msg, t_data *data)
 {
@@ -42,17 +142,46 @@ void	ft_error(char *err_msg, t_data *data)
 	exit (EXIT_FAILURE);
 }
 
+void	free_philo_list(t_philo *list)
+{
+	t_philo	*aux;
+
+	if (!list)
+		return ;
+	while (list)
+	{
+		/* Should I free it or close it? */
+		if (list->thread)
+			free (list->thread);
+		aux = list->next;
+		free (list);
+		list = aux;
+	}
+}
+
+void	free_philo_data(t_data *data)
+{
+	if (!data)
+		return ;
+	/* Should I free it or close it? */
+	if (data->mutex)
+		free (data->mutex);
+	if (data->forks)
+		free (data->forks);
+	if (data->philo_list)
+		free_philo_list(data->philo_list);
+	free (data);
+}
+
 int	main(int argc, char *argv[])
 {
 	t_data	*data;
-	int		exit_status;
 
 	if (argc != 5 && argc != 6)
 		ft_error("Not enough args!", NULL);
-	exit_status = initialize_philo_data(argc, argv, &data);
-	if (exit_status != SUCCESS)
-		ft_error("Failure initializing the data structure.", data);
-	exit_status = run_simulation(data);
+	initialize_philo_data(argc, argv, &data);
+	launch_philo_threads(data);
+	monitor_philosophers(data);
 	free_philo_data(data);
-	return (exit_status);
+	return (EXIT_SUCCESS);
 }
